80f7151dbbebd264ceeae60341ea4411518ea69a
fleawhat <fleawhat@gmail.com>
2016-03-10 20:01:18 +0800

    save last status
    last input and checkbox

--- SimpleUI/app/src/main/java/com/example/sampleui/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/sampleui/simpleui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.sampleui.simpleui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,19 +18,32 @@ public class MainActivity extends AppCompatActivity { //繼承自Activity類別
     TextView textView;
     EditText editText;
     CheckBox hideCheckBox;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) { //覆寫Activity類別的onCreate方法
         super.onCreate(savedInstanceState); // 一定要加入呼叫父類別onCreate方法的敘述
         setContentView(R.layout.activity_main); // 指定這個元件使用的畫面配置資源
+
+
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);
         hideCheckBox = (CheckBox)findViewById(R.id.checkBox);
+
+        sp = getSharedPreferences("setting", Context.MODE_PRIVATE);//取得setting內容
+        editor = sp.edit();//在白紙上寫字
+
+        editText.setText(sp.getString("editText", ""));//如果找不到editText，就會輸入空白字串
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
-                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)
-                {
+                editor.putString("editText", editText.getText().toString());//輸入字串給editor
+                editor.apply();//儲存
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
                     submit(v);
                     return true;
                 }
@@ -38,12 +54,23 @@ public class MainActivity extends AppCompatActivity { //繼承自Activity類別
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE)
-                {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
                     submit(v);
                     return true;
                 }
                 return false;
+            }
+        });
+
+        hideCheckBox =  (CheckBox)findViewById(R.id.checkBox);
+
+        hideCheckBox.setChecked(sp.getBoolean("hideCheckBox", false));//儲存上個checkbox的狀態
+
+        hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hideCheckBox", hideCheckBox.isChecked());
+                editor.apply();
             }
         });
     }
